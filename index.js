a191a2e8dfb42fa70cf310ed2c81274a392d2b9c
Corentin Dupont <corentin.dupont@gmail.com>
2018-01-23 17:43:00 +0100

    clean

--- index.js
+++ index.js
@@ -9,17 +9,8 @@ const httpProxy = require('http-proxy');
 
 var apiProxy = httpProxy.createProxyServer()
 
-
 //Create app and router
 const app = express();
-const router = express.Router();
-
-//Log every API call 
-router.use(function(req, res, next) {
-    console.log('%s URI: %s PATH: %s', req.method, req.url, req.path);
-    console.log('  Headers:' + JSON.stringify(req.headers));
-    next();
-});
 
 //Create memory store and session to store user credentials server side
 var memoryStore = new session.MemoryStore();
@@ -49,22 +40,18 @@ app.use(keycloak.middleware({
 
 console.log(JSON.stringify(keycloak))
 
-//install routes
+//install proxy over Kibana
+//it will redirect to Keycloak for login
 app.all("/*", keycloak.protect(), function(req, res){
-  console.log(req.method + ' : ' + req.url);
-  console.log('kibanaUrl: ' + config.kibanaUrl);
-  //req.url = req.url.replace('/kibana','');
   apiProxy.web(req, res, { target: config.kibanaUrl });
   apiProxy.on('error', function(e) { 
     console.log('Error: ' + e);
-     
   });
-  console.log('after');
 });
 
 async function run() {
     await app.listen(config.port);
-    console.log('Listening on port ', config.port);
+    console.log('Kibana proxy listening on port ', config.port);
 }
 
 run();
